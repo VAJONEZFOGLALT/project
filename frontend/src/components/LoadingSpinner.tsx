42dc31da2780cd6a11e162d8590ce85a465c0a39
bazs <zuhnhvh@gmail.com>
2026-03-03 15:17:21 +0100

    Localize loading spinner label

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,10 +1,13 @@
 import styles from './LoadingSpinner.module.css';
+import { useTranslation } from 'react-i18next';
 
 export const LoadingSpinner = ({ fullScreen = false }: { fullScreen?: boolean }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={`${styles.spinner} ${fullScreen ? styles.fullScreen : ''}`}>
       <div className={styles.loader}></div>
-      <p>Loading...</p>
+      <p>{t('common.loading')}</p>
     </div>
   );
 };
